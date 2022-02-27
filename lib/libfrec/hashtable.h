07b050642fb25e64b924a1858d47a52c5dd7bade
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2022-02-27 14:38:24 +0100

    Reformat, restructure, and document hashtable

--- lib/libfrec/hashtable.h
+++ lib/libfrec/hashtable.h
@@ -5,25 +5,27 @@
 
 #include <sys/types.h>
 
-#define HASH_OK         0
-#define HASH_UPDATED    1
-#define HASH_FAIL       2
-#define HASH_FULL       3
-#define HASH_NOTFOUND   4
+#define HASH_OK 0
+#define HASH_UPDATED 1
+#define HASH_FAIL 2
+#define HASH_FULL 3
+#define HASH_NOTFOUND 4
 
-#define HASHSTEP(x,c) (((x << 5) + x) + (c))
+#define HASHSTEP(x, c) (((x << 5) + x) + (c))
 
-typedef struct {
-  void *key;
-  void *value;
+typedef struct
+{
+    void *key;
+    void *value;
 } hashtable_entry;
 
-typedef struct {
-  size_t key_size;
-  size_t table_size;
-  size_t usage;
-  size_t value_size;
-  hashtable_entry **entries;
+typedef struct
+{
+    size_t tbl_size;
+    size_t key_size;
+    size_t val_size;
+    size_t usage;
+    hashtable_entry **entries;
 } hashtable;
 
 void hashtable_free(hashtable *);
@@ -32,4 +34,4 @@ hashtable *hashtable_init(size_t, size_t, size_t);
 int hashtable_put(hashtable *, const void *, const void *);
 int hashtable_remove(hashtable *, const void *);
 
-#endif	/* HASHTABLE.H */
+#endif /* HASHTABLE.H */
