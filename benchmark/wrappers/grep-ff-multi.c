6bcfb765ade9cfe0f1f4d9138a11a69cb883451a
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2022-10-15 13:11:54 +0200

    Add firstfrom multipattern matcher

---
+++ benchmark/wrappers/grep-ff-multi.c
@@ -0,0 +1,114 @@
+/*
+ * Grep Multi: Builds an executable that can be used to find every match
+ * in a text with multiple given patterns.
+ */
+
+#include <frec.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <err.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_REGEX_PATTERNS 8
+#define ERROR_BUFFER_SIZE 512
+
+int
+main(int argc, char *argv[])
+{
+    char **patterns = malloc(sizeof(char *) * MAX_REGEX_PATTERNS);
+    int pattern_cnt = 0;
+    int cflags = 0;
+    int eflags = 0;
+
+    cflags |= REG_EXTENDED;
+
+    // Process command line arguments.
+    int c;
+    while ((c = getopt(argc, argv, "e:l")) != -1) {
+        switch (c) {
+            // Save as the pattern to search for.
+            case 'e':
+                patterns[pattern_cnt] = malloc(sizeof(char) * (strlen(optarg) + 1));
+                strcpy(patterns[pattern_cnt], optarg);
+                
+                pattern_cnt++;
+                break;
+            // This means we have to set REG_NEWLINE.
+            case 'l':
+                cflags |= REG_NEWLINE;
+                break;
+        }
+    }
+
+    // Shift arguments after we processed every flag.
+    argv += optind;
+    argc -= optind;
+
+    // If the pattern isn't set, or no argument remains, exit.
+    if (pattern_cnt == 0 || argc == 0) {
+        printf("Usage: grep -e PATTERN [-e PATTERN...] [-l] INPUT\n");
+        exit(2);
+    }
+
+    // Compile pattern.
+    int ret;
+
+    mfrec_t preg;
+    ret = frec_mregcomp(&preg, pattern_cnt, (const char **) patterns, cflags);
+
+    // Handle potential errors.
+    if (ret != 0) {
+        char buffer[ERROR_BUFFER_SIZE + 1];
+
+        int which = 0;
+        frec_mregerror(ret, &preg, &which, buffer, ERROR_BUFFER_SIZE);
+        errx(2, "%s : %s", patterns[which], buffer);
+    }
+
+    // Check if file exists.
+    struct stat st;
+    if (stat(*argv, &st) != 0 || !S_ISREG(st.st_mode)) {
+        errx(2, "Invalid file: %s", *argv);
+    }
+
+    // Open file and create read buffer.
+    int file = open(*argv, O_RDONLY);
+    if (file < 0) {
+        errx(2, "Invalid file: %s", *argv);
+    }
+
+    int map_flags = MAP_PRIVATE;
+    char *buffer = mmap(NULL, st.st_size, PROT_READ, map_flags, file, 0);
+    if (buffer == MAP_FAILED) {
+        errx(2, "Invalid file buffer from file: %s", *argv);
+    }
+
+    // Execute regex matching.
+    frec_match_t pmatch;
+
+    ret = frec_mregnexec(&preg, buffer, st.st_size, 1, &pmatch, eflags);
+
+    // If no more matches were found, break.
+    if (ret == REG_NOMATCH) {
+        return 1;
+    }
+
+    // On any other error, print the error then exit.
+    if (ret != 0) {
+        char buffer[ERROR_BUFFER_SIZE + 1];
+
+        frec_mregerror(ret, &preg, NULL, buffer, ERROR_BUFFER_SIZE);
+        errx(2, "%s", buffer);
+        return 1;
+    }
+
+    // Otherwise we print the match.
+    printf("%ld (%ld %ld)\n", pmatch.pattern_id, pmatch.soffset, pmatch.eoffset);
+
+    return !(ret > 0);
+}
